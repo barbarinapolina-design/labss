a1963cacc0fd700757aa7a7eeabcbebc049d1438
Lina <barbarinapolina@gmail.com>
2026-06-03 12:09:45 +0300

    Add files via upload

---
+++ Лр_3/2/unit1.pas
@@ -0,0 +1,31 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Calendar, EditBtn;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Calendar1: TCalendar;
+    DateEdit1: TDateEdit;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
